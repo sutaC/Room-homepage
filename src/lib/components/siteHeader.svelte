0255bf32529c67bbf7fd68524515e7ce2411aae7
Antoni Siekański <catus.pl@gmail.com>
2023-07-03 21:11:09 +0200

    Mobile nav modal

--- src/lib/components/siteHeader.svelte
+++ src/lib/components/siteHeader.svelte
@@ -1,7 +1,16 @@
+<script lang="ts">
+	import { slide } from 'svelte/transition';
+
+	let navActive = false;
+</script>
+
 <header>
-	<button class="hamburger"><img src="/images/icon-hamburger.svg" alt="Open navigation" /></button>
-	<nav>
-		<button>
+	<button class="hamburger" on:click={() => (navActive = true)}
+		><img src="/images/icon-hamburger.svg" alt="Open navigation" /></button
+	>
+
+	<nav class:navActive>
+		<button on:click={() => (navActive = false)} class="close">
 			<img src="/images/icon-close.svg" alt="Close" />
 		</button>
 		<a href="/">Home</a>
@@ -9,6 +18,7 @@
 		<a href="/">About</a>
 		<a href="/">Contact</a>
 	</nav>
+	<div class="backdrop" />
 	<img src="/images/logo.svg" alt="Logo" />
 </header>
 
@@ -27,14 +37,54 @@
 		display: none;
 	}
 
-	.hamburger {
+	.navActive {
+		display: block;
+		position: fixed;
+		top: 0;
+		left: 0;
+
+		padding: 3.5rem;
+		width: 100%;
+		background-color: var(--clr-white);
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 10%;
+
+		z-index: 100;
+	}
+	.navActive + .backdrop {
+		width: 100vw;
+		height: 100vh;
+		position: fixed;
+		inset: 0;
+		background-color: var(--clr-black);
+		opacity: 0.4;
+	}
+
+	nav > a {
+		text-transform: lowercase;
+		text-decoration: none;
+		color: var(--clr-black);
+		font-weight: bolder;
+		font-size: 1.25rem;
+	}
+
+	button {
+		background-color: transparent;
+		border: none;
+	}
+
+	button.hamburger {
 		position: absolute;
 
 		left: 1.5rem;
 		top: 50%;
 		translate: 0 -50%;
+	}
 
-		background-color: transparent;
-		border: none;
+	button.close {
+		margin-right: 2rem;
 	}
 </style>
